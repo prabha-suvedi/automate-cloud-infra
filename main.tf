25f7d8e1a5b285864e6333a18dc9404a12607560
prabha suwedi <61641518+prabha-suvedi@users.noreply.github.com>
2025-01-04 20:06:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   # Do not hardcode credentials. Use environment variables or IAM roles for access
 }
 
-# Create a VPC
+
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16" # VPC CIDR block
   enable_dns_support   = true
@@ -14,7 +14,7 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-# Create an Internet Gateway (IGW) and attach it to the VPC
+
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -23,7 +23,7 @@ resource "aws_internet_gateway" "main_igw" {
   }
 }
 
-# Create a custom route table for the VPC
+
 resource "aws_route_table" "main_route_table" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -38,7 +38,7 @@ resource "aws_route_table" "main_route_table" {
   }
 }
 
-# Create a Subnet and associate it with the route table
+
 resource "aws_subnet" "main_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
@@ -49,13 +49,13 @@ resource "aws_subnet" "main_subnet" {
   }
 }
 
-# Associate the subnet with the route table
+
 resource "aws_route_table_association" "main_subnet_association" {
   subnet_id      = aws_subnet.main_subnet.id
   route_table_id = aws_route_table.main_route_table.id
 }
 
-# Create a Security Group
+
 resource "aws_security_group" "example_sg" {
   name        = "example-security-group"
   description = "Allow HTTP, HTTPS, and SSH traffic"
@@ -94,7 +94,7 @@ resource "aws_security_group" "example_sg" {
   }
 }
 
-# Create an Elastic Network Interface (ENI) in the subnet
+
 resource "aws_network_interface" "example_eni" {
   subnet_id       = aws_subnet.main_subnet.id          # Reference to the subnet where this ENI will be created
   private_ips     = ["10.0.1.10"]                      # Assign a private IP address within the subnet CIDR range
@@ -105,7 +105,7 @@ resource "aws_network_interface" "example_eni" {
   }
 }
 
-# Allocate an Elastic IP
+
 resource "aws_eip" "example_eip" {
   domain = "vpc" # This is the correct way to specify that the EIP is for a VPC
 
@@ -115,19 +115,19 @@ resource "aws_eip" "example_eip" {
 }
 
 
-# Attach the Elastic IP to the ENI
+
 resource "aws_network_interface_attachment" "eni_attachment" {
   instance_id          = aws_instance.example_instance.id # Use instance_id to ensure attachment
   network_interface_id = aws_network_interface.example_eni.id
   device_index         = 0
 }
 
-# Create an EC2 instance and attach the ENI
+
 resource "aws_instance" "example_instance" {
   ami           = "ami-02df5cb5ad97983ba" # Replace with a valid AMI ID
   instance_type = "t3.micro"
 
-  # Attach the ENI to the EC2 instance
+  
   network_interface {
     network_interface_id = aws_network_interface.example_eni.id
     device_index         = 1 # Always 0 for the primary network interface
@@ -138,7 +138,7 @@ resource "aws_instance" "example_instance" {
   }
 }
 
-# Variables for AWS Credentials (ensure this is securely handled)
+
 variable "aws_access_key" {
   description = "AKIA2OAJUD2C5TUHHGX5"
   type        = string
